cfe2f22daaa2a9891b5c49499358247dbe49853d
krledmno1@gmail.com <krledmno1@gmail.com@b407db6c-0c91-1484-e248-8a2ec9398d2e>
2012-05-28 15:34:33 +0000

    Print bayes network

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -4,15 +4,20 @@
  *  Created on: May 14, 2012
  *      Author: krle
  */
+#include "BayesNetwork/BayesNetwork.h"
 #include "IndexTree/BranchNode.h"
 #include "IndexTree/LeafNode.h"
 #include "IndexTree/TreeNode.h"
 #include "BayesNetwork/CPT.h"
+#include "Data/DataTable.h"
 
 
 int main(int argc, char* argv[])
 {
+	char* bla=NULL;
+	DataTable d(bla);
 	CPT a;
+	BayesNetwork b(&d);
 	return 0;
 }
 
